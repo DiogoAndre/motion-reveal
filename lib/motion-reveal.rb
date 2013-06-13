599ce4337da722fbc4220949ca9b24993ffa1264
Diogo <diogo@regattapix.com>
2013-06-13 09:03:24 -0300

    Removing commented out lines

--- lib/motion-reveal.rb
+++ lib/motion-reveal.rb
@@ -2,24 +2,6 @@ unless defined?(Motion::Project::Config)
   raise "This file must be required within a RubyMotion project Rakefile."
 end
 
-# def create_launcher(files)
-#   # --- based on motion-testflight
-#   launcher_code = <<EOF
-# # created by motion-sparkinspector
-#   if Object.const_defined?('SparkInspector')
-#     SparkInspector.enableObservation
-#   end
-# EOF
-
-#   launcher_file = './app/sparkinspector_launcher.rb'
-#   if !File.exist?(launcher_file) or File.read(launcher_file) != launcher_code
-#     File.open(launcher_file, 'w') { |io| io.write(launcher_code) }
-#   end
-#   files = files.flatten
-#   files << launcher_file unless files.find { |x| File.expand_path(x) == File.expand_path(launcher_file) }
-
-# end
-
 Motion::Project::App.setup do |app|
   app.development do
     if File.exist? ('/Applications/Reveal.app')
