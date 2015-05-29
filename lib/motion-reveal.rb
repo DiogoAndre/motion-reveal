a91c4448fc28b98562affd6ce6def333bbe4496c
y8 <y8@ya.ru>
2015-05-29 17:59:39 +0200

    Fix Reveal 1.5 'Undefined symbols' build error

--- lib/motion-reveal.rb
+++ lib/motion-reveal.rb
@@ -5,6 +5,8 @@ end
 Motion::Project::App.setup do |app|
   app.development do
     if File.exist? ('/Applications/Reveal.app')
+      # Fixes build issues with Reveal 1.5
+      app.libs += ['/usr/lib/libz.dylib', '/usr/lib/libc++.dylib']
       app.vendor_project('/Applications/Reveal.app/Contents/SharedSupport/iOS-Libraries/Reveal.framework', :static, :products => ['Reveal'], :cflags => '-ObjC')
       app.frameworks << 'CFNetwork'
       app.frameworks << 'QuartzCore'
